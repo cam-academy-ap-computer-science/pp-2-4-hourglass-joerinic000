58076b889b84447f1017c921e8f67e5774181271
joerinic000 <joerinic000@CAM-LAB-12.bgsd.k12.wa.us>
2019-10-08 07:51:54 -0700

    Day 1 commit

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,7 +1,26 @@
-/*
-
+/*Pseudocode for Hourglass
+	The constant should be used to determine the height, or number of lines, in each half of the hourglass, HALF_HEIGHT
+	
+	Shape:
+	draw Line
+	draw Top
+	draw Centre
+	draw Bottom
+	draw Line
+	
+	draw Line:
+	Print a "|"
+	In the for loop, multiply the constant by 2
+	In the loop, print a " until amount is equal to (HALF_HEIGHT*2)
+	Print a "|"
+	
+	draw Top:
+	
+	
 */
 
 public class Hourglass {
-
+	public static void main(String[] args) {
+			
+	}
 }
